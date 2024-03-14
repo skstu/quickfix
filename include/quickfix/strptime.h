9b732239dbd0c7b25d05f977828deb631278a6b5
memade <k34ubll5@outlook.com>
2024-03-14 14:14:20 +0800

    updated

---
+++ include/quickfix/strptime.h
@@ -0,0 +1,7 @@
+#ifdef _WIN32
+
+extern "C"
+  char *
+  strptime( const char *buf, const char *fmt, struct tm *tm );
+
+#endif
---
+++ src/C++/test/TestHelper.cpp
@@ -0,0 +1,14 @@
+#ifdef _WIN32
+#include "stdafx.h"
+#else
+#include "config.h"
+#endif
+
+#include "TestHelper.h"
+
+namespace FIX
+{
+  const uint16_t TestSettings::port = 54321;
+  FIX::SessionSettings TestSettings::sessionSettings;
+  std::string TestSettings::postgreSQLUser = "";
+}
